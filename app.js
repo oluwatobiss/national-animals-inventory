2ae1e17c5b323e0467b9fc33b3b817887a8675e4
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-09-11 13:52:33 +0100

    style: Display each national animal's information as cards

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 app.listen(port, () =>
